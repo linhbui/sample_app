c1d394f4ead8fd780bc2fd5407451ac54b592252
Linh Bui <linhbui@colgate.edu>
2014-07-28 23:27:46 -0500

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  # Add Capybara gem to run RSpec
+  config.include Capybara::DSL
 end
